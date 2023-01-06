a69d0325ec3cc7b4073879d954e16b1ecf4506ed
coderSathesBala <sathes352@gmail.com>
2023-01-05 23:13:00 -0500

    addGravityToObject

--- index.js
+++ index.js
@@ -4,21 +4,46 @@ const c = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
+
+const gravity = 0.5
 class Player {
     constructor() {
         this.position = {
             x: 100,
             y: 100
         }
-        this.width = 100
-        this.height = 100
+
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        this.width = 30
+        this.height = 30
     }
 
     draw() {
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+
+    update() {
+        this.position.y += this.velocity.y
+        this.draw()
+        this.velocity.y += gravity
+        if(this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity
+        } else {this.velocity.y = 0}
+
+    }
+    
 }
 
 const player = new Player()
 
-player.draw()
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, canvas.width, canvas.height)
+    player.update()
+}
+
+animate()
