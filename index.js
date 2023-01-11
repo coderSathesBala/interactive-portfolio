b9fda99af62a6ac364ff187b2458476cd3ecd6ce
coderSathesBala <sathes352@gmail.com>
2023-01-10 23:34:34 -0500

    FinishedMultipleScrollingPlatforms

--- index.js
+++ index.js
@@ -48,47 +48,11 @@ const keys = {
     }
 }
 
-function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0,0, canvas.width, canvas.height)
-    player.update()
-    platform.draw()
-    
-    if(keys.right.pressed
-        && player.position.x < 400
-        ) {
-        player.velocity.x = 5
-    } else if(keys.left.pressed && player.position.x > 100) {player.velocity.x = -5} 
-    else {
-        console.log(platform.position.x)
-        player.velocity.x = 0
-        // if(platform.position.x <= 200) {
-
-            
-            if(keys.right.pressed) {
-                platform.position.x -= 5
-            } else if (keys.left.pressed) {
-                platform.position.x += 5
-            }
-        // }
-    }
-
-// platform collision detection
-    if (player.position.y + player.height 
-        <= platform.position.y && 
-        player.position.y + player.height + player.velocity.y
-        >= platform.position.y && player.position.x + player.width 
-        >= platform.position.x 
-        && player.position.x <= platform.position.x + platform.width) {
-            player.velocity.y = 0
-    }
-}
-
 class Platform {
-    constructor() {
+    constructor({x, y}) {
         this.position = {
-            x: 200,
-            y: 700
+            x: x,
+            y: y
         }
         
         this.width = 200
@@ -101,7 +65,58 @@ class Platform {
 }
 
 const player = new Player()
-const platform = new Platform()
+const platforms = [new Platform({x: 200, y: 200}), new Platform({x: 400, y: 400})]
+
+let scrollOffset = 0
+
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0,0, canvas.width, canvas.height)
+    player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
+    
+    if(keys.right.pressed
+        && player.position.x < 400
+        ) {
+        player.velocity.x = 5
+    } else if(keys.left.pressed && player.position.x > 100) {player.velocity.x = -5} 
+    else {
+        player.velocity.x = 0
+
+            
+            if(keys.right.pressed) {
+                scrollOffset += 5
+                platforms.forEach(platform => {
+                    platform.position.x -= 5
+                })
+            } else if (keys.left.pressed) {
+                scrollOffset -=5
+                platforms.forEach(platform => {
+                    platform.position.x += 5
+                })
+            }
+    }
+
+    console.log(scrollOffset)
+
+// platform collision detection
+    platforms.forEach(platform => {
+        if (player.position.y + player.height 
+            <= platform.position.y && 
+            player.position.y + player.height + player.velocity.y
+            >= platform.position.y && player.position.x + player.width 
+            >= platform.position.x 
+            && player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    })
+
+    if(scrollOffset > 2000){
+        console.log('You Win')
+    }
+}
 
 animate()
 
